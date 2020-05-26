69edf58f55bc984d02d351d079e80cf84dec23dd
Yannis Bouhamou <yannis.bouhamou@hotmail.fr>
2020-05-26 15:46:10 +0200

    finish

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,13 +1,14 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.all
-    @offersgeoco = @offers.users.geocoded # returns offers with coordinates
+    @users = User.all
+    @offersgeoco = Offer.where(user: @users.geocoded ) # returns offers with coordinates
 
     @markers = @offersgeoco.map do |offer|
       {
         lat: offer.user.latitude,
         lng: offer.user.longitude,
-        # infoWindow: render_to_string(partial: "info_window", locals: { offer: offer })
+        infoWindow: render_to_string(partial: "info_window", locals: { offer: offer }),
+        image_url: helpers.asset_url('../assets/images/logo.png')
       }
     end
   end
