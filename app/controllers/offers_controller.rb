3318fc569c92ea3460a5054d805f9a230777dd17
Yannis Bouhamou <yannis.bouhamou@hotmail.fr>
2020-05-27 01:25:18 +0200

    design

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,6 +7,7 @@ class OffersController < ApplicationController
     @offersgeoco = policy_scope(Offer).where(user_id: @users.map(&:id)) # returns offers with coordinates
 
     elsif params[:address].present? && params[:radius].present? && params[:product].present?
+      @product = Product.find(params[:product])
       @users = User.near(params[:address], params[:radius].to_i)
       @offersgeoco = policy_scope(Offer).where(user_id: @users.map(&:id)).where(product: params[:product]) # returns offers with coordinates
 
@@ -20,14 +21,13 @@ class OffersController < ApplicationController
     @offersgeoco.each do |offer|
       @stock += offer.quantity
     end
-     @product = Product.find(params[:product])
 
     @markers = @offersgeoco.map do |offer|
       {
         lat: offer.user.latitude,
         lng: offer.user.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { offer: offer }),
-        image_url: helpers.asset_url('thread.png')
+        image_url: helpers.asset_url('sewing-machine.png')
       }
     end
   end
