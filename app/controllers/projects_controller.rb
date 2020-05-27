50f5a6de6bbbec069b26c66490c105d55c7dd33a
Jeremy Barbedienne <jeremybarbedienne@gmail.com>
2020-05-27 14:44:57 +0200

    changement back + conflits pour destroy ...

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = current_user
