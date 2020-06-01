317586240cfe00347b232b0ae45790f34dc9760f
Yannis Bouhamou <yannis.bouhamou@hotmail.fr>
2020-06-01 08:29:46 +0200

    quick win + travail back-end project/matches

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,7 @@ class ProjectsController < ApplicationController
     @product = Product.find(project_params[:product_id])
     @offers = Offer.order('price ASC, quantity DESC').find(params[:offers_id]) if params[:offers_id]
     @matches_quantity = []
+    @usertocontact = User.near(current_user.address, 10) #cela représente tous les mask makers à 10km à la ronde du client
   end
 
   def create
@@ -21,6 +22,13 @@ class ProjectsController < ApplicationController
       params[:matches][:offer_ids].each do |offer_id|
         Match.create(project: @project, offer_id: offer_id.to_i, quantity: params[:matches]["#{:match_quantity_offer_}"+"#{offer_id}"])
       end
+
+      @usertocontact = User.near(current_user.address, 10) #cela représente tous les masks makers situés à 10km du client
+     #@usertocontact.map(&:id).each do |id|
+      #ing = Offer.create(product: @product, quantity: 0, user_id: id )
+      #Match.create(project: @project, quantity: 0, offer_id: ing.id )
+      #end
+
       redirect_to project_path(@project)
     else
       render :new
