22cfa567ff47806eca5934cf93bed0848779da63
Yannis Bouhamou <yannis.bouhamou@hotmail.fr>
2020-06-03 19:34:54 +0200

    add the markers to the dashboard map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,26 +10,24 @@ class PagesController < ApplicationController
     @offers = current_user.offers
     @user_projects = Project.where(user: current_user)
 
-    @marker_center = {
+    @marker_center_dash = {
       lat: current_user.latitude,
       lng: current_user.longitude,
       image_url: helpers.asset_url('location.png')
     }
 
-    current_user.projects.each do |project|
-      @markers = project.matches.each do |match|
-      {
-        lat: match.offer.user.latitude,
-        lng: match.offer.user.longitude,
-        image_url: helpers.asset_url('sewing-machine.png')
-      }
-      end
-    end
-
     @friends = current_user.projects.map do |project|
       project.matches.map { |match| match.offer.user }
     end
+
     @friends = @friends.flatten
+    @markers_dash = @friends.map do |friend|
+      {
+        lat: friend.latitude,
+        lng: friend.longitude,
+        image_url: helpers.asset_url('sewing-machine.png')
+      }
+    end
 
   end
 end
