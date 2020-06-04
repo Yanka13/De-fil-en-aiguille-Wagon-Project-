5fa9b4f7e64c83208097a79013ba3bab7bd51445
Sorayabenhaddou <soraya-b55@hotmail.fr>
2020-06-04 12:27:08 +0200

    en cours

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,7 +3,9 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('search');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput }).configure({
+     countries: ['fr'] // only search in the United States, the rest of the settings are unchanged: we're still searching for cities in German.
+    });
   }
 };
 
